d3e58762d3163f054ecc7ee66d508f9d165e192e
miklik72 <miklik@miklik.cz>
2018-08-22 22:31:36 +0200

    0.4.2 - corrections for ESP32
    set better impuls treshold 70 -> 100
    replace float in IRQ handler by int
    add IRAM_ATTR for irqhandler
    rename BIT0 and 1 to TBIT0 and 1

--- SensorT25.cpp
+++ SensorT25.cpp
@@ -7,13 +7,15 @@ v0.2.0 - copy from 0.1.9 and remove coments and debug data
 v0.2.1 - add new funcion - enable, disable and remove some variables
 v0.2.2 - rename data array to buffer, and use data array for data per chanel
 v0.2.3 - save data directly to data array
-v0.2.4 - rename get functions to decode and create new getfunctions and some isFFF functions 
+v0.2.4 - rename get functions to decode and create new getfunctions and some isFFF functions
 v0.2.5 - correct some daty types
 v0.2.6 - set prefix _ for global private objects
 v0.3.0 - do it like class
 v0.3.1 - change all to static
 v0.3.2 - rename class to SensorT25
 v0.4.0 - save it like library
+v0.4.1 - rename BIT0 and 1 to TBIT0 and 1, user IRAM_ATTR for IRQ handler
+v0.4.2 - ESP32 is not supporting float in IRQ handler, use int instead
 */
 
 #include <SensorT25.h>
@@ -21,30 +23,31 @@ v0.4.0 - save it like library
 unsigned long SensorT25::_buffer[BUFF_ROW] = {0L,0L,0L}; // raw data buffer
 boolean SensorT25::_valid[SENSCOUNT] = {false,false,false}; // valid dta forsensor
 byte SensorT25::_sid[SENSCOUNT];           // sensor ID
-float SensorT25::_temperature[SENSCOUNT];  // temperature
+//float SensorT25::_temperature[SENSCOUNT];  // temperature
+int SensorT25::_itemperature[SENSCOUNT];   // temperature
 unsigned long SensorT25::_time[SENSCOUNT]; // last set time
 boolean SensorT25::_start = LOW;             // start data reading flag
-boolean SensorT25::_space = LOW;             // space is starting 
+boolean SensorT25::_space = LOW;             // space is starting
 boolean SensorT25::_bit0 = LOW;
 boolean SensorT25::_bit1 = LOW;
 byte SensorT25::_bits = 0;                   // bits counter in data word
 byte SensorT25::_repeat = 0;                 // counter for repeated reading
 unsigned long SensorT25::_lastTime = 0;      // variable for last time point
 byte SensorT25::_irq_pin;                    // number of used IRQ PIN
-// methods    
-void SensorT25::enable(byte irq_pin)               //enable data reading - INT0,INT1 name, number 2 or 3 for UNO 
-{ 
-  _irq_pin = irq_pin;             
+// methods
+void SensorT25::enable(byte irq_pin)               //enable data reading - INT0,INT1 name, number 2 or 3 for UNO
+{
+  _irq_pin = irq_pin;
   pinMode(irq_pin, INPUT_PULLUP);                                       // input for RF receiver
   attachInterrupt(digitalPinToInterrupt(irq_pin), _irqHandler, CHANGE); // IRQ handler for RF receiver
 }
-             
+
 void SensorT25::disable(byte irq_pin)                      //disable daata reading
 {
   detachInterrupt(digitalPinToInterrupt(irq_pin));         // stop IRQ
 }
 
-// are data valid for channel       
+// are data valid for channel
 boolean SensorT25::isValid(byte channel)
 {
   return (isValidChannel(channel) ? _valid[channel] : false);
@@ -62,19 +65,20 @@ void SensorT25::setInValid(byte channel)
 //get sensor ID for channel
 byte SensorT25::getSID(byte channel)
 {
-  return (isValidChannel(channel) ? _sid[channel] : 0);  
+  return (isValidChannel(channel) ? _sid[channel] : 0);
 }
 
 //get temperature for channel
 float SensorT25::getTemperature(byte channel)
 {
-  return (isValidChannel(channel) ? _temperature[channel] : 0);
+  //return (isValidChannel(channel) ? _temperature[channel] : 0);
+  return (isValidChannel(channel) ? float(_itemperature[channel])/10 : 0);
 }
 
 //get temperature value age in s
 long SensorT25::getValueAge(byte channel)
 {
-  return (isValidChannel(channel) ? (millis() - _time[channel])/1000 : 0);  
+  return (isValidChannel(channel) ? (millis() - _time[channel])/1000 : 0);
 }
 
 // channel number validation
@@ -83,7 +87,7 @@ boolean SensorT25::isValidChannel(byte channel)
   return ( channel >= 0 && channel < SENSCOUNT);
 }
 
-// return specified bits from sensor word 
+// return specified bits from sensor word
 unsigned int SensorT25::_decodeBits(unsigned long buffer, unsigned long mask, byte shift )
 {
     return ((buffer & mask) >> shift);
@@ -102,41 +106,49 @@ byte SensorT25::_decodeChanel(unsigned long buffer)
 }
 
 // decode temperature from data word
-float SensorT25::_decodeTemp(unsigned long buffer)
+// float SensorT25::_decodeTemp(unsigned long buffer)
+// {
+//   int t = _decodeBits(buffer, TEM_MASK, TEM_SHIFT);
+//   if (t > 2047) {t = t - 4095;}
+//   return float(t)/10;
+// }
+
+// decode temperature from data word
+int SensorT25::_idecodeTemp(unsigned long buffer)
 {
   int t = _decodeBits(buffer, TEM_MASK, TEM_SHIFT);
   if (t > 2047) {t = t - 4095;}
-  return float(t)/10;
+  return t;
 }
 
-// IRQ handler for decode bits
+// IRQ handler for decode bits ...
 void SensorT25::_irqHandler()
 {
   unsigned long currentTime = micros();        // shot time
   int duration = currentTime - _lastTime;       // calculate duration
   _lastTime = currentTime;                      // memory curent time
   boolean state = digitalRead(_irq_pin);        // read input status
-  if (!state)                                  // goes from HIGH to LOW 
-  {                                            
+  if (!state)                                  // goes from HIGH to LOW
+  {
       _space = _isImpuls(duration - IMPULSE, ITRESHOLD);     // if impuls time is valid
-  }    
+  }
   else if (_space)                              // goes from LOW to HIGH
-  {                                        
+  {
     if (!_start)                                // start flag is false
-    {                                         
+    {
       _start = _isImpuls(duration - PAUSE, STRESHOLD);  // start temperature data it was pause signal
-    } 
+    }
     else                                            // start flag is true
     {
-      _bit0 = _isImpuls(duration - BIT0, STRESHOLD);  // is it bit 0
-      _bit1 = _isImpuls(duration - BIT1, STRESHOLD);  // is it bit 1
+      _bit0 = _isImpuls(duration - TBIT0, STRESHOLD);  // is it bit 0
+      _bit1 = _isImpuls(duration - TBIT1, STRESHOLD);  // is it bit 1
       if(_bit0 ^ _bit1)                               // XOR / only one is true
       {
         _bit1 && bitSet(_buffer[_repeat],DATA_LONG - _bits - 1);   // write bit 1
         _bit0 && bitClear(_buffer[_repeat],DATA_LONG - _bits - 1); // write bit 0
         _bits++;                                                // increment bit counter
-      } 
-      else                                          // both or nothing bit 
+      }
+      else                                          // both or nothing bit
       {
         _resetTWord ();                              // reset reading
       }
@@ -144,7 +156,7 @@ void SensorT25::_irqHandler()
       {
         if(_repeat > 0)                              // is it temp sentence hienr then 0
         {
-          if (_buffer[_repeat] != _buffer[_repeat-1])       // current temp word is differnt than previous 
+          if (_buffer[_repeat] != _buffer[_repeat-1])       // current temp word is differnt than previous
           {
             _resetTWord ();                          // reset reading
           }
@@ -152,19 +164,19 @@ void SensorT25::_irqHandler()
         _repeat++;                                   // increment temp word reading
         _newTWord();                                 // reset counters and flags
       }
-    }  
+    }
   }
- 
+
  // write valid data to data array
  if (_repeat >= BUFF_ROW)                            // buffer is full
   {
   byte ch = (_decodeChanel(_buffer[0]));                 // set channel
   _valid[ch] = true;                                 // set data valid
   _sid[ch] = (_decodeSID(_buffer[0])); //sensor ID        // save sensor ID
-  _temperature[ch] = (_decodeTemp(_buffer[0]));           // save temperature
+  _itemperature[ch] = (_idecodeTemp(_buffer[0]));           // save temperature
   _time[ch] = millis();                              //save data timestamp
   _resetTWord ();                                    // reset reading
-  }  
+  }
 }
 
 void SensorT25::_resetTWord ()                                  // reset reading
--- SensorT25.h
+++ SensorT25.h
@@ -7,13 +7,16 @@ v0.2.0 - copy from 0.1.9 and remove coments and debug data
 v0.2.1 - add new funcion - enable, disable and remove some variables
 v0.2.2 - rename data array to buffer, and use data array for data per chanel
 v0.2.3 - save data directly to data array
-v0.2.4 - rename get functions to decode and create new getfunctions and some isFFF functions 
+v0.2.4 - rename get functions to decode and create new getfunctions and some isFFF functions
 v0.2.5 - correct some daty types
 v0.2.6 - set prefix _ for global private objects
 v0.3.0 - do it like class
 v0.3.1 - change all to static
 v0.3.2 - rename class to SensorT25
 v0.4.0 - save it like library
+v0.4.1 - rename BIT0 and 1 to TBIT0 and 1, user IRAM_ATTR for IRQ handler
+v0.4.2 - ESP32 is not supporting float in IRQ handler, use int for array
+v0.4.2 - treshold 100 for impuls
 */
 
 #ifndef SensorT25_h
@@ -24,10 +27,10 @@ v0.4.0 - save it like library
 #define SENSCOUNT 3           // Sensors count
 #define START  9000           // start space time
 #define PAUSE  4020           // space between sentences
-#define BIT1   1980           // 1
-#define BIT0   1000           // 0
+#define TBIT1   1980           // 1
+#define TBIT0   1000           // 0
 #define IMPULSE 410           // impulse
-#define ITRESHOLD 70          // impulse treshold +- for duration
+#define ITRESHOLD 100          // impulse treshold +- for duration
 #define STRESHOLD 50          // space treshold +- for duration
 #define BUFF_ROW 3             // row in buffer for data
 #define DATA_LONG 32           // buffer long 32bits, last 4 bits are droped
@@ -48,28 +51,30 @@ class SensorT25 {
     // sensors data - index 0 for channel 1,...
     static boolean _valid[SENSCOUNT]; // valid dta forsensor
     static byte _sid[SENSCOUNT];           // sensor ID
-    static float _temperature[SENSCOUNT];  // temperature
+    //static float _temperature[SENSCOUNT];  // temperature
+    static int _itemperature[SENSCOUNT];   // temperature in integer (float * 10)
     static unsigned long _time[SENSCOUNT]; // last set time
     static boolean _start;             // start data reading flag
-    static boolean _space;             // space is starting 
+    static boolean _space;             // space is starting
     static boolean _bit0;
     static boolean _bit1;
     static byte _bits;                   // bits counter in data word
     static byte _repeat;                 // counter for repeated reading
     static unsigned long _lastTime;      // variable for last time point
     static byte _irq_pin;
-   
+
     static unsigned int _decodeBits(unsigned long, unsigned long, byte );
     static byte _decodeSID(unsigned long);
     static byte _decodeChanel(unsigned long);
-    static float _decodeTemp(unsigned long);
+    //static float _decodeTemp(unsigned long);
+    static int _idecodeTemp(unsigned long);
     static void _irqHandler();
     static void _resetTWord ();                                  // reset reading
     static void _newTWord ();                                    // reset temp word counter
     static boolean _isImpuls(int, byte);       // is it valid impulse
-    
+
   public:
-    static void enable(byte);                    //enable data reading - INT0,INT1 name, number 2 or 3 for UNO 
+    static void enable(byte);                    //enable data reading - INT0,INT1 name, number 2 or 3 for UNO
     static void disable(byte);                   //disable daata reading
     static boolean isValid(byte);
     static void setInValid(byte);
